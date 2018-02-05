b5f449c2c7bfa685008d1c0d02a1260fec8130c2
Cuel <Cuel@users.noreply.github.com>
2018-02-05 20:13:44 +0100

    remove vanilla missions pbo

---
+++ extra/remove_vanilla_missions/config.cpp
@@ -0,0 +1,14 @@
+class CfgPatches {
+    class fpa_no_vanilla_missions {
+        author = "Cuel";
+        name = "";
+        url = "";
+        version = 1;
+        units[] = {};
+        requiredVersion = 1.78;
+        authors[] = {"Cuel"};
+        requiredAddons[] = {"fpa_main"};
+    };
+};
+
+#include "CfgMissions.hpp"
