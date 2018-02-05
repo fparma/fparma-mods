c3ace28c2a2b0033d1f21eadc475f5418cfe1399
Cuel <Cuel@users.noreply.github.com>
2018-02-05 11:04:30 +0100

    Disable vanilla missions

---
+++ addons/main/CfgMissions.hpp
@@ -0,0 +1,57 @@
+
+class CfgMissions {
+	class MPMissions {
+		delete Showcase_Combined_arms;
+		delete Showcase_FiringFromVehicles;
+		delete MP_Bootcamp_01;
+		delete MP_Bootcamp_02;
+		delete MP_COOP_m01;
+		delete MP_COOP_m02;
+		delete MP_COOP_m03;
+		delete MP_COOP_m04;
+		delete MP_COOP_m05;
+		delete MP_COOP_m06;
+		delete MP_COOP_m07;
+		delete MP_COOP_m08;
+		delete MP_GroundSupport01;
+		delete MP_GroundSupport02;
+		delete MP_GroundSupport03;
+		delete MP_GroundSupport04;
+		delete MP_GroundSupport05;
+		delete MP_Marksmen_01;
+		delete MP_End_Game_02;
+		delete MP_End_Game_03;
+		delete MP_End_Game_04;
+		delete MP_End_Game_05;
+		delete MP_ZR_8_Karts01;
+		delete MP_ZR_8_Karts02;
+		delete MP_ZSC_m10;
+		delete MP_ZSC_m11;
+		delete MP_ZGM_m11;
+		delete MP_ZGM_m11_EAST;
+		delete MP_ZGM_m11_WEST;
+		delete MP_ZGM_m11_GUER;
+		delete MP_ZGM_m12;
+		delete MP_ZGM_m12_EAST;
+		delete MP_ZGM_m12_WEST;
+		delete MP_ZGM_m12_GUER;
+		delete MP_ZGM_m13;
+		delete MP_ZGM_m13_EAST;
+		delete MP_ZGM_m13_WEST;
+		delete MP_ZGM_m13_GUER;
+		delete MP_ZGM_m14;
+		delete MP_ZGM_m14_EAST;
+		delete MP_ZGM_m14_WEST;
+		delete MP_ZGM_m14_GUER;
+
+		class Apex {
+			delete EXP_m01;
+			delete EXP_m02;
+			delete EXP_m03;
+			delete EXP_m04;
+			delete EXP_m05;
+			delete EXP_m06;
+			delete EXP_m07;
+		};
+	};
+};
