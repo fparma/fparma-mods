2bf6c10c138934ee21ebda47fae45cc25ac57984
diwako <diwako@gmail.com>
2021-06-20 23:09:36 +0200

    Update RPG ammo to allow to be shot at infantry

--- addons/ai/CfgAmmo.hpp
+++ addons/ai/CfgAmmo.hpp
@@ -1,7 +1,26 @@
 class CfgAmmo {
-	class RocketBase;
-	class R_PG7_F : RocketBase {
-		airLock = 1;
-		allowAgainstInfantry = 1;
-	};
+    class RocketBase;
+    class R_PG7_F : RocketBase {
+        aiAmmoUsageFlags = "64 + 128 + 256 + 512";
+        airLock = 1;
+        allowAgainstInfantry = 1;
+        cost = 0.1;
+        effectsMissile = "missile3";
+        whistleDist = 30;
+    };
+    class M_SPG9_HEAT : RocketBase {
+        aiAmmoUsageFlags = "64 + 128 + 512";
+        allowAgainstInfantry = 1;
+    };
+    class R_PG32V_F : RocketBase {
+        aiAmmoUsageFlags = "64 + 128 + 256 + 512";
+        airLock = 1;
+        allowAgainstInfantry = 1;
+        cost = 40;
+    };
+    class R_MRAAWS_HEAT_F : RocketBase {
+        aiAmmoUsageFlags = "64 + 128 + 512";
+        allowAgainstInfantry = 1;
+        cost = 40;
+    };
 };
