1a58c351d009102aee40e63c98eead032798bebd
diwako <diwako@gmail.com>
2021-06-20 23:09:59 +0200

    update cba macros

--- addons/logging/XEH_preInitServer.sqf
+++ addons/logging/XEH_preInitServer.sqf
@@ -1,6 +1,6 @@
 #include "script_component.hpp"
 
 GVAR(id) = format ["%1///%2///%3///%4", worldName, briefingName, dateToNumber date, round random 10000];
-call COMPILE_FILE(sock);
+call COMPILE_SCRIPT(sock);
 
 INFO_1("Mission UID: %1", GVAR(id));
--- addons/saveMarkers/cfgEventhandlers.hpp
+++ addons/saveMarkers/cfgEventhandlers.hpp
@@ -1,12 +1,12 @@
 class Extended_PreInit_EventHandlers {
     class ADDON {
-        init = QUOTE(call COMPILE_FILE(XEH_preInit));
-        clientInit = QUOTE(call COMPILE_FILE(XEH_preClientInit));
+        init = QUOTE(call COMPILE_SCRIPT(XEH_preInit));
+        clientInit = QUOTE(call COMPILE_SCRIPT(XEH_preClientInit));
     };
 };
 
 class Extended_PostInit_EventHandlers {
     class ADDON {
-        clientInit = QUOTE(call COMPILE_FILE(XEH_postInitClient));
+        clientInit = QUOTE(call COMPILE_SCRIPT(XEH_postInitClient));
     };
 };
