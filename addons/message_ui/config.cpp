6e3eb88c44f21a57ddd26a1a50263d16fa41bcf0
Perondas <thomas@perondas.dev>
2023-12-03 15:42:15 +0100

    Continued fixing HEMTT errors

--- addons/message_ui/config.cpp
+++ addons/message_ui/config.cpp
@@ -2,7 +2,7 @@
 
 class CfgPatches {
     class ADDON {
-        name = COMPONENT_NAME;
+        name = QUOTE(COMPONENT_NAME);
         units[] = {};
         weapons[] = {};
         requiredVersion = REQUIRED_VERSION;
